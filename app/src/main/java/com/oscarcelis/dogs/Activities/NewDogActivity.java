b95ef207f999f595b7003133da74674406aa26b2
OscarCelis <oscarcelis6@gmail.com>
2019-03-22 20:11:17 -0500

    MMA versión 1.5 Restablecer contraseña en firebase. Botón de logout (SignOut) firebase funcionando en actividad principal. Carga y visualización de imagen directamente de Firebase Storage en formulario nuevo canino. La sesión permanece abierta incluso si el dispositivo es reiniciado. Los datos (texto e imagen por separado) del formulario nuevo canino ya se almacenan en Firebase Realtime Database. ScrollView en formulario principal funciona ok. Los campos son validados antes de enviar a la base de datos.

--- app/src/main/java/com/oscarcelis/dogs/Activities/NewDogActivity.java
+++ app/src/main/java/com/oscarcelis/dogs/Activities/NewDogActivity.java
@@ -1,23 +1,29 @@
 package com.oscarcelis.dogs.activities;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.ImageViewCompat;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,32 +34,49 @@ import com.oscarcelis.dogs.MainActivity;
 import com.oscarcelis.dogs.R;
 import com.squareup.picasso.Picasso;
 
-import java.net.URI;
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NewDogActivity extends AppCompatActivity {
+public class NewDogActivity extends AppCompatActivity implements View.OnClickListener {
 
     private StorageReference mRefStorage;
+
     private static final int GALLERY_INTENT = 1;
     private static final int ELIMINARIMAGEN = 1;
 
+    private TextView edtUrlImagenCanino;
+    private TextView txtGenero;
+    private TextView txtEspecialidad;
+    private TextView txtTitulo;
     private TextView txtEmailUsuarioActual;
+    Calendar C = Calendar.getInstance();
+    private EditText edtFechaNacimientoCanino;
 
     private EditText edtNombreCanino;
     private EditText edtChipCanino;
-    private EditText edtRazaCanino;
-    private EditText edtSexoCanino;
-    private EditText edtFechaNacimientoCanino;
-    private EditText edtColorCanino;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioMasculino;
+    private RadioButton radioFemenino;
+
+    private int dia, mes, anio;
+    static final int DATE_ID = 'C';
+
     private EditText edtSenasCanino;
     private EditText edtProcedenciaCanino;
-    private EditText edtEspecialidadCanino;
 
+    private Button btnImgStorage;
     private Button btnGuardarDatosNuevoCanino;
     private Button btnCerrarSesion;
-    private Button btnImgStorage;
-    private Button btnBorrarFoto;
+
+
+    private AutoCompleteTextView autoCompleteRaza;
+    private AutoCompleteTextView autoCompleteColor;
+
+    private Spinner spinnerEspecialidad;
 
     private ImageView imgMostrarImgCanino;
 
@@ -62,7 +85,7 @@ public class NewDogActivity extends AppCompatActivity {
     DatabaseReference mRootReference;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_dog);
 
@@ -71,25 +94,44 @@ public class NewDogActivity extends AppCompatActivity {
 
         FirebaseAuth datosUsuario = firebaseAuth.getInstance();
 
+        btnImgStorage = findViewById(R.id.btnImgStorage);
+        imgMostrarImgCanino = findViewById(R.id.imgMostrarImgCanino);
+
         edtNombreCanino = findViewById(R.id.edtNombreCanino);
         edtChipCanino = findViewById(R.id.edtChip);
-        edtRazaCanino = findViewById(R.id.edtRazaCanino);
-        edtSexoCanino = findViewById(R.id.edtSexoCanino);
+
+        radioGroup = findViewById(R.id.radioGeneroGroup);
+        radioMasculino = findViewById(R.id.radioButtonMasculino);
+        radioFemenino = findViewById(R.id.radioButtonFemenino);
+
+        txtGenero = findViewById(R.id.txtGenero);
+        txtEspecialidad = findViewById(R.id.txtEspecialidad);
+        txtTitulo = findViewById(R.id.txtTitulo);
+
+        //Inicia fecha de nacimiento
+        dia = C.get(Calendar.DAY_OF_MONTH);
+        mes = C.get(Calendar.MONTH);
+        anio = C.get(Calendar.YEAR);
         edtFechaNacimientoCanino = findViewById(R.id.edtFechaNacimientoCanino);
-        edtColorCanino = findViewById(R.id.edtColorCanino);
+        //Termina fecha de nacimiento
+
         edtSenasCanino = findViewById(R.id.edtSenasCanino);
         edtProcedenciaCanino = findViewById(R.id.edtProcedenciaCanino);
-        edtEspecialidadCanino = findViewById(R.id.edtEspecialidadCanino);
+
+        edtUrlImagenCanino = findViewById(R.id.edtUrlImagenCanino);
+        edtUrlImagenCanino.setVisibility(View.INVISIBLE);
 
         txtEmailUsuarioActual = findViewById(R.id.txtEmailUsuarioActual);
-        txtEmailUsuarioActual.setText("Estás autenticado como "  + datosUsuario.getCurrentUser().getEmail());
+        txtEmailUsuarioActual.setText("Estás autenticado como " + datosUsuario.getCurrentUser().getEmail());
 
-        imgMostrarImgCanino = findViewById(R.id.imgMostrarImgCanino);
+        autoCompleteRaza = findViewById(R.id.autRazaCanino);
+        autoCompleteColor = findViewById(R.id.autColorCanino);
+
+        spinnerEspecialidad = findViewById(R.id.spinnerEspecialidadCanino);
 
         btnGuardarDatosNuevoCanino = findViewById(R.id.btnGuardarDatosNuevoCanino);
         btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
-        btnImgStorage = findViewById(R.id.btnPickFoto);
-        btnBorrarFoto = findViewById(R.id.btnBorrarFoto);
+
 
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,9 +143,19 @@ public class NewDogActivity extends AppCompatActivity {
         btnGuardarDatosNuevoCanino.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               guardarDatosCanino();
+                guardarDatosCanino();
             }
         });
+
+        imgMostrarImgCanino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+
 
         btnImgStorage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,6 +166,75 @@ public class NewDogActivity extends AppCompatActivity {
             }
         });
 
+
+        edtFechaNacimientoCanino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_ID);
+            }
+        });
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (radioMasculino.isChecked()) {
+                    txtGenero.setText("M");
+                }
+                if (radioFemenino.isChecked()) {
+                    txtGenero.setText("F");
+                }
+            }
+        });
+
+        //Autocomplete para razas
+        autoCompleteRaza = findViewById(R.id.autRazaCanino);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.RAZAS, android.R.layout.simple_list_item_1);
+
+        autoCompleteRaza.setAdapter(adapter);
+
+
+        //Autocomplete para colores del canino
+        autoCompleteColor = findViewById(R.id.autColorCanino);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.COLORES_CANINOS, android.R.layout.simple_list_item_1);
+
+        autoCompleteColor.setAdapter(adapter1);
+
+
+        //Spinner especialidades
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array .ESPECIALIDADES, android.R.layout.simple_spinner_item);
+        spinnerEspecialidad.setAdapter(adapter2);
+
+
+
+    }
+
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id){
+            case DATE_ID:
+                return new DatePickerDialog(this, DateSetListener, anio, mes, dia);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener DateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                  anio = year;
+                  mes = month;
+                  dia = dayOfMonth;
+                  colocarFecha();
+        }
+    };
+
+    private void colocarFecha(){
+            edtFechaNacimientoCanino.setText((anio)+"-"+(mes)+"-"+(dia));
           }
 
     @Override
@@ -129,8 +250,9 @@ public class NewDogActivity extends AppCompatActivity {
             filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    imgMostrarImgCanino.setEnabled(false);
                     btnImgStorage.setEnabled(false);
-                    btnImgStorage.setText("" + mRefStorage.child("fotos_caninos").child(uri.getLastPathSegment()));
+                    edtUrlImagenCanino.setText("" + mRefStorage.child("fotos_caninos").child(uri.getLastPathSegment()));
 
                     Uri mostrarImagen = uri;
                     Picasso.get()
@@ -150,38 +272,102 @@ public class NewDogActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
     }
 
+    private void validarChip(){
+        String chipCanino = edtChipCanino.getText().toString();
+
+    }
 
 
-        private void guardarDatosCanino(){
 
+
+
+
+
+
+
+    private void guardarDatosCanino(){
+
+            String urlImagenCanino = edtUrlImagenCanino.getText().toString();
             String nombre = edtNombreCanino.getText().toString();
             String chipCanino = edtChipCanino.getText().toString();
-            String razaCanino = edtRazaCanino.getText().toString();
-            String sexoCanino = edtSexoCanino.getText().toString();
+            String razaCanino = autoCompleteRaza.getText().toString();
+
+            //String sexoCanino = edtSexoCanino.getText().toString();
+            String genero = txtGenero.getText().toString();
+
             String nacimientoCanino = edtFechaNacimientoCanino.getText().toString();
-            String colorCanino = edtColorCanino.getText().toString();
+            String colorCanino = autoCompleteColor.getText().toString();
             String senasCanino = edtSenasCanino.getText().toString();
             String procedenciaCanino = edtProcedenciaCanino.getText().toString();
-            String especialidadCanino = edtEspecialidadCanino.getText().toString();
+            String especialidad = spinnerEspecialidad.getSelectedItem().toString();
+
+
+        if(TextUtils.isEmpty(chipCanino)){
+            Toast.makeText(this,"Ingrese un chip",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if(TextUtils.isEmpty(nombre)){
+            Toast.makeText(this,"Ingrese un nombre",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+         if(TextUtils.isEmpty(razaCanino)){
+            Toast.makeText(this, "Ingrese una raza", Toast.LENGTH_SHORT).show();
+            return;
+            }
+
+
+        if(!radioFemenino.isChecked() && !radioMasculino.isChecked()) {
+            Toast.makeText(this, "Elija un género", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+            if(TextUtils.isEmpty(procedenciaCanino)){
+                Toast.makeText(this, "Ingrese procedencia", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(TextUtils.isEmpty(colorCanino)){
+                Toast.makeText(this, "Ingrese un color", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(TextUtils.isEmpty(senasCanino)){
+                Toast.makeText(this, "Si no hay señales, escriba NINGUNA", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
 
             Map<String, Object> credencialesCanino = new HashMap<>();
 
+            credencialesCanino.put("urlImagenCanino", urlImagenCanino);
             credencialesCanino.put("nombre", nombre);
             credencialesCanino.put("chip", chipCanino);
             credencialesCanino.put("raza", razaCanino);
-            credencialesCanino.put("sexo", sexoCanino);
+
+            credencialesCanino.put("genero", genero);
+
             credencialesCanino.put("fecha_nacimiento", nacimientoCanino);
             credencialesCanino.put("color", colorCanino);
             credencialesCanino.put("senales", senasCanino);
             credencialesCanino.put("procedencia", procedenciaCanino);
-            credencialesCanino.put("especialidad", especialidadCanino);
+            credencialesCanino.put("especialidad", especialidad);
+
 
             mRootReference.child("Canino").push().setValue(credencialesCanino);
 
         }
 
 
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.edtNombreCanino:
 
+        }
+    }
 }
